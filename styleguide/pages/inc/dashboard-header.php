f539e0cb18b05831cd5de6d8a5c802511ec8da8a
Adam Marsh <am@ui-design-engineering.com>
2022-01-31 13:12:24 -0800

    new topnav

--- styleguide/pages/inc/dashboard-header.php
+++ styleguide/pages/inc/dashboard-header.php
@@ -1,9 +1,13 @@
   <div class="row">
-      <div class="col-1 txt-white"><a href="javascript:history.back()"><img class="top-logo"
-                  src="../dist/img/logo.jpg" /></a>
-      </div>
-      <div class="col-10" id="main-tabs">
+
+      <div class="col-12" id="main-tabs">
           <ul class="nav nav-tabs nav-tabs-primary">
+              <li>
+                  <a href="javascript:history.back()"><img class="top-logo" src="../dist/img/logo.jpg" /></a>
+              </li>
+              <li>
+                  &nbsp;
+              </li>
               <li class="nav-item nav-link nojump active" data-toggle="tab" data-target="#dashboard-panel">
                   <i class="now-ui-icons objects_umbrella-13"></i> Dashboard
               </li>
@@ -19,63 +23,88 @@
               <li class="nav-item nav-link nojump" data-toggle="tab" data-target="#search">
                   <i class="now-ui-icons location_world"></i> Search
               </li>
-              <li class="nav-item nav-link nojump" data-toggle="tab" data-target="#add">
-                  <i class="now-ui-icons gestures_tap-01"></i> New +
+              <li class="nav-item nav-link nojump">
+                  <div class="dropdown">
+                      <button class="btn btn-secondary top">+New</button>
+                      <div class="dropdown-content">
+                          <a href="#">Phone Call</a>
+                          <a href="#">Task</a>
+                          <a href="#">Contact</a>
+                      </div>
+                  </div>
+              </li>
+              <li style="margin-right:20%;">
+
+                  icons
+
+
               </li>
           </ul>
-          <div class="card nopad">
-              <div class="card-body txt-black">
-                  <!-- Tab panes -->
-                  <div class="tab-content nopad">
-                      <div class="tab-pane active" id="dashboard-panel" role="tabpanel">
-                          <p>"Everywhere but in Valencia people are mowing lawns, planting fences, tuning
-                              engines, counting receipts, baking bread, hanging laundry, keeping the wheel
-                              turning. Tradition, not convention, holds us here in Valencia . In the
-                              valley, the wheel does not turn. We keep it still, anchor it in the beach,
-                              in the brackish water of Vero bay. </p>
-                          <h3>Possum</h3>
-                          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
-                              commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
-                              risus. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus
-                              ac cursus
-                              commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
-                              risus. Etiam
-                              porta sem malesuada magna mollis euismod. Donec sed odio dui. Etiam
-                              porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                      </div>
-                      <div class="tab-pane" id="work" role="tabpanel">
-                          <p> I could watch them glide inches above the sea...inches above the seatop at
-                              unfathomable speeds. The littleness of the bird, defiant and inimitable
-                              against the rushing backdrop is equally unfathomable, yet the little bird
-                              dismisses it without a sense of ego or bravado. Mass and energy, sprinting
-                              elements of wind and water exist outside the wheel where ambition and
-                              progress is overruled by freedom and instinct. </p>
-                          <h3>Tater</h3>
-                          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
-                              commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
-                              risus. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus
-                              ac cursus
-                              commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
-                              risus. Etiam
-                              porta sem malesuada magna mollis euismod. Donec sed odio dui. Etiam
-                              porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                      </div>
-                      <div class="tab-pane" id="reports" role="tabpanel">
-                          <p>reports </p>
-                      </div>
-                      <div class="tab-pane" id="tools" role="tabpanel">
-                          <p>tools </p>
-                      </div>
-                      <div class="tab-pane" id="search" role="tabpanel">
-                          <p> search </p>
-                      </div>
-                      <div class="tab-pane" id="add" role="tabpanel">
-                          <p> add + </p>
-                      </div>
+          <section class="navigation">
+              <div class="card nopad mt20">
+                  <div class="card-body txt-black">
+                      <!-- Tab panes -->
+                      <div class="tab-content nopad">
+                          <div class="tab-pane active" id="dashboard-panel" role="tabpanel">
+                              <h3>Dashboard</h3>
+                              <p>"Everywhere but in Valencia people are mowing lawns, planting fences, tuning
+                                  engines, counting receipts, baking bread, hanging laundry, keeping the wheel
+                                  turning. Tradition, not convention, holds us here in Valencia . In the
+                                  valley, the wheel does not turn. We keep it still, anchor it in the beach,
+                                  in the brackish water of Vero bay. </p>
+                              <h3>Possum</h3>
+                              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+                                  commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                                  risus. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus
+                                  ac cursus
+                                  commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                                  risus. Etiam
+                                  porta sem malesuada magna mollis euismod. Donec sed odio dui. Etiam
+                                  porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                          </div>
+                          <div class="tab-pane" id="work" role="tabpanel">
+                              <h3>My Work</h3>
+                              <p> I could watch them glide inches above the sea...inches above the seatop at
+                                  unfathomable speeds. The littleness of the bird, defiant and inimitable
+                                  against the rushing backdrop is equally unfathomable, yet the little bird
+                                  dismisses it without a sense of ego or bravado. Mass and energy, sprinting
+                                  elements of wind and water exist outside the wheel where ambition and
+                                  progress is overruled by freedom and instinct. </p>
+                              <h3>Tater</h3>
+                              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+                                  commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                                  risus. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus
+                                  ac cursus
+                                  commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                                  risus. Etiam
+                                  porta sem malesuada magna mollis euismod. Donec sed odio dui. Etiam
+                                  porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                          </div>
+                          <div class="tab-pane" id="reports" role="tabpanel">
+                              <h3>Reports</h3>
+                              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus
+                                  commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                                  risus. Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus
+                                  ac cursus
+                                  commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                                  risus. Etiam
+                                  porta sem malesuada magna mollis euismod. Donec sed odio dui. Etiam
+                                  porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                          </div>
+                          <div class="tab-pane" id="tools" role="tabpanel">
+                              <h3>Tools </h3>
+                          </div>
+                          <div class="tab-pane" id="search" role="tabpanel">
+                              <h3>Search </h3>
+                          </div>
+                          <div class="tab-pane" id="add" role="tabpanel">
+                              <p> add + </p>
+                          </div>
 
+                      </div>
                   </div>
               </div>
-          </div>
+          </section>
       </div>
-      <div class="col-1 txt-white">icons</div>
+
   </div>
