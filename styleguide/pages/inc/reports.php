be15a7b50377f4826f682bb4e42604a5b6b3f37b
Adam Marsh <am@ui-design-engineering.com>
2022-02-23 07:47:14 -0800

    SSIs for tab content

---
+++ styleguide/pages/inc/reports.php
@@ -0,0 +1,19 @@
+ <div class="card narrow opacity">
+     <div class="card-body txt-black">
+         <h1>Reports</h1>
+         <section class="container wow fadeInUp">
+             <div class="card">
+                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac
+                     cursus
+                     commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                     risus. Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+                     tellus
+                     ac cursus
+                     commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                     risus. Etiam
+                     porta sem malesuada magna mollis euismod. Donec sed odio dui. Etiam
+                     porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+             </div>
+         </section>
+     </div>
+ </div>
---
+++ styleguide/pages/inc/tools.php
@@ -0,0 +1,10 @@
+ <div class="card narrow opacity">
+                    <div class="card-body txt-black">
+                        <h1>Tools </h1>
+                        <section class="container wow fadeInUp">
+                            <div class="card">
+                                copy
+                            </div>
+                        </section>
+                    </div>
+                </div>
